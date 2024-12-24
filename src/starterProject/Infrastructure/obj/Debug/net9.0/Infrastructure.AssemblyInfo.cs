7881093632fa92426bbe56407892486403eb6d43
Ahmet Akif Arabalı <ahmetakifrbl@gmail.com>
2024-12-25 01:29:18 +0300

    .

--- src/starterProject/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ src/starterProject/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa4725dc4e60ebdd1337793110c8d35bba42245")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f593d3b7148a24fd689eb8fca7bbb17c67d278a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
