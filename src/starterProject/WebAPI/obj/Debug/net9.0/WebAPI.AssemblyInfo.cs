3934b7786ded6a7f6da991979072df63254df33f
Ali Taş <alitas7875@gmail.com>
2024-12-25 09:32:22 +0300

    .

--- src/starterProject/WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ src/starterProject/WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f593d3b7148a24fd689eb8fca7bbb17c67d278a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7881093632fa92426bbe56407892486403eb6d43")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
